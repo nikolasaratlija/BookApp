a31b6c474aa892daa1510b19e16dfaa9af58f27c
nikola.saratlija <saratlijanikola@gmail.com>
2022-06-14 10:09:58 +0200

    test

--- resources/js/components/BookListingContainer.js
+++ resources/js/components/BookListingContainer.js
@@ -2,10 +2,10 @@ import * as React from 'react'
 import { BookListing } from './BookListing'
 
 
-export class BookListingContainer extends React.Component{
-    render() {
-        return <div className="row gy-5">
-            {[1,2,3,4,5].map(() => <BookListing></BookListing>)}
+export const BookListingContainer = () => {
+    return (
+        <div className="row gy-5">
+            {[1, 2, 3, 4, 5].forEach(() => <BookListing></BookListing>)}
         </div>
-    }
+    )
 }
