6239e1c2e7f6c7984979ba0c3447049f298f514f
nikola.saratlija <saratlijanikola@gmail.com>
2022-06-14 17:53:55 +0200

    ignoring compiled files - css and js

--- resources/js/components/booklisting/BookListingContainer.jsx
+++ resources/js/components/booklisting/BookListingContainer.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { useState } from 'react/cjs/react.production.min';
 import { BookListing } from './BookListing'
 
 
@@ -8,7 +9,7 @@ const BookListingContainer = () => {
             <div className="container px-5 px-lg-5 mt-5">
                 <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
 
-                    {[...Array(10).keys()].map(number => <BookListing key={number} title={"Book " + number}/>)}
+                    {[...Array(10).keys()].map(number => <BookListing key={number} title={"Book " + number} />)}
 
                 </div>
             </div>
